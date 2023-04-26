4e7518928f746b6e055868bc5998ee2b9b5136ff
Folawiyo <folawiyofarouq@gmail.com>
2023-04-26 08:33:47 -0700

    Task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Component 6-base_geometry.
+Generates a class.
+"""
+
+
+class BaseGeometry:
+    """Class with common case method."""
+
+    def area(self):
+        """Lifts an Exception with the message
+        'area() is not used'.
+        """
+
+        raise Exception('area() is not implemented')
