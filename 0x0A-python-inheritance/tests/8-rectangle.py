324ffe29d5d23b4bbc0682e30c855583856ebf72
Folawiyo <folawiyofarouq@gmail.com>
2023-04-26 08:48:32 -0700

    Task 8

---
+++ 0x0A-python-inheritance/tests/8-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module 8-rectangle.
+"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Represents a rectangle.
+    Inherits from BaseGeometry.
+    """
+
+    def __init__(self, width, height):
+        """Initializes an instance."""
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
