e78d72fbb5e7daab801779e5324c78381607394f
Folawiyo <folawiyofarouq@gmail.com>
2023-04-26 07:57:53 -0700

    Task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Accommodate the is_kind_of_class function
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Accurate if obj is an occasion or take_over from a_class, otherwise not accurate"""
+    return (isinstance(obj, a_class))
