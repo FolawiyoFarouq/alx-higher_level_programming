c10b44c46dd6b8d332fe37e0140ada9fe908dbb4
FolawiyoFarouq <folawiyofarouq@gmail.com>
2023-05-17 05:31:20 -0700

    Task 9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- creates second table and add multiple rows
+-- F A R O U Q
+CREATE TABLE IF NOT EXISTS second_table (id INT,
+name VARCHAR(256),
+score INT);
+INSERT INTO second_table (id, name, score)
+VALUES (1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8);
