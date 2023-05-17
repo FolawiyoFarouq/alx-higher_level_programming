ca149d20dbe1d1085d763409cc8ff65cf451980f
FolawiyoFarouq <folawiyofarouq@gmail.com>
2023-05-17 03:58:12 -0700

    Task 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,10 +1,6 @@
-#!/bin/bash
-
-DB_NAME="$1"
-mysql -h localhost -u root -p "$DB_NAME" <<EOF
+-- create table with id type INT
+-- F A R O U Q
 CREATE TABLE IF NOT EXISTS first_table (
   id INT,
   name VARCHAR(256)
-);
-EOF
-
+  );
