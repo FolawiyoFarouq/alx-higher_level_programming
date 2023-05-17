9937c86144ff4405703a1ca4ac8d0355601d16f0
FolawiyoFarouq <folawiyofarouq@gmail.com>
2023-05-17 06:51:31 -0700

    Task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,4 @@
 -- F A R O U Q
 -- create user and set password
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
